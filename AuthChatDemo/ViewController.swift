9b5957768b89f668242048617d03aa4201ded23c
Jonathan Guthrie <jono@perfect.org>
2017-07-05 01:09:06 -0400

    Initial Commit

---
+++ AuthChatDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AuthChatDemo
+//
+//  Created by Jonathan Guthrie on 2017-07-05.
+//  Copyright © 2017 iamjono.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
